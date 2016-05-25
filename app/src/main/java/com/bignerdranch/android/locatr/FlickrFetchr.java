d9cd20d9a8f1c472f7f39dd3696b39a96316d81f
Eddie Rodriguez <eddie.rodriguez3@lc.cuny.edu>
2016-05-25 07:03:07 -0400

    Added API keys

--- app/src/main/java/com/bignerdranch/android/locatr/FlickrFetchr.java
+++ app/src/main/java/com/bignerdranch/android/locatr/FlickrFetchr.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class FlickrFetchr {
     private static final String TAG = "FlickrFetchr";
 
-    private static final String API_KEY = "REPLACE_ME_WITH_A_REAL_KEY";
+    private static final String API_KEY = "594ea391d4b397ef171011f293af2d30";
     private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
     private static final String SEARCH_METHOD = "flickr.photos.search";
     private static final Uri ENDPOINT = Uri
